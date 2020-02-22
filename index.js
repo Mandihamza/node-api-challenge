88522c80172adb205a4eb32f3233f83dc38586a6
mandihamza <mandihamza@gmail.com>
2020-02-22 11:21:59 -0700

    Imports projectRouter at /api/projects

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
-const welcomeRouter = require("./welcome/welcome-router")
+const welcomeRouter = require("./welcome/welcome-router.js")
+const projectRouter = require("./data/helpers/projectRouter.js")
 
 const server = express()
 // .env file includes PORT="4000" variable
@@ -8,6 +9,7 @@ const port = process.env.PORT || 4000
 
 server.use(express.json())
 server.use("/", welcomeRouter)
+server.use("/api/projects", projectRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
