2b4bfbc726fd0ab2a857cfe557029453b64d67fd
mandihamza <mandihamza@gmail.com>
2020-02-24 17:30:43 -0700

    Imports action router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const welcomeRouter = require("./welcome/welcome-router.js")
 const projectRouter = require("./data/helpers/projectRouter.js")
+const actionRouter = require("./data/helpers/actionRouter")
 
 const server = express()
 // .env file includes PORT="4000" variable
@@ -10,6 +11,7 @@ const port = process.env.PORT || 4000
 server.use(express.json())
 server.use("/", welcomeRouter)
 server.use("/api/projects", projectRouter)
+server.use("/api/actions", actionRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
